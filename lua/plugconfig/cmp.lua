8e426494c4276d68ced46ff60a8b6385665fef25
redpz <sv_redpz@proton.me>
2023-05-30 19:08:30 +0100

    Removed buffer CMP

--- lua/plugconfig/cmp.lua
+++ lua/plugconfig/cmp.lua
@@ -51,7 +51,7 @@ cmp.setup({
   sources = cmp.config.sources({
     { name = "nvim_lsp" },
     { name = "luasnip" },
-    { name = "buffer" },
+    -- { name = "buffer" },
     { name = "path" },
   }),
   experimental = {
