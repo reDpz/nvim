c4f15d3dfeb00c614cd4da678731e82df89e9c90
reDpz <sv_redpz@proton.me>
2023-06-01 14:51:48 +0100

    update

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -15,10 +15,17 @@ keymap('n', 'q', '<cmd>bd<CR>')
 keymap('i', '<C-s>', '<cmd>wa<CR>')
 keymap('n', '<C-s>', '<cmd>wa<CR>')
 
+-- exit
+keymap('i', '<C-x>', '<cmd>wq<CR>')
+keymap('n', '<C-x>', '<cmd>wq<CR>')
+
 -- write file
 keymap("n", "ZW", "<cmd>wa<CR>")
 
-
+-- move down and up with with alt+j/k
+keymap('i', '<A-j>', '<Down>')
+keymap('i', '<A-k>', '<Up>')
+-- The reason as to why I use these is because I have a 60% keyboard, and I like it. Sometimes I might want to go up or down without exiting insert mode so therefore I would use alt + hjkl but then that would put me into normal mode.
 
 -- opens nvim-tree
 keymap('n', '<leader>e', '<cmd>NvimTreeToggle<CR>')
