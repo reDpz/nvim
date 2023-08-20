73e7e6c3733099bb7e587d689ea08b6ffb700c40
reDpz <sv_redpz@proton.me>
2023-08-20 21:19:46 +0100

    update

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,17 @@
+return{
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>fr', builtin.search_history, {})
+        vim.keymap.set('n', '<leader>of', builtin.oldfiles, {})
+        require('telescope').setup {
+            defaults = {
+                theme = "center",
+            }
+        }
+    end
+
+}
