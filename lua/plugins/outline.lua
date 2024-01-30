c22cf7f846ff72bc4776a0101d22f1dd43891353
reDpz <sv_redpz@proton.me>
2024-01-30 13:58:54 +0000

    themes

--- lua/plugins/outline.lua
+++ lua/plugins/outline.lua
@@ -17,11 +17,11 @@ return {
         split_command = nil,
 
         -- Percentage or integer of columns
-        width = 15,
+        width = 45,
         -- Whether width is relative to the total width of nvim
         -- When relative_width = true, this means take 25% of the total
         -- screen width for outline window.
-        relative_width = true,
+        relative_width = false,
 
         -- Auto close the outline window if goto_location is triggered and not for
         -- peek_location
