8101ea60a151a9371a8642c676d7a77031b08416
reDpz <sv_redpz@proton.me>
2024-02-03 01:44:31 +0000

    more lazy loading

--- lua/plugins/html.lua
+++ lua/plugins/html.lua
@@ -1,12 +1,14 @@
 return{
     {
         'windwp/nvim-ts-autotag',
+        lazy = true,
         config = function ()
             require('nvim-ts-autotag').setup()
         end
     },
     {
         "ziontee113/color-picker.nvim",
+        lazy = true,
         config = function()
             require("color-picker")
         end,
