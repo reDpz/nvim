3f3b26cbde68cdbb7aa412d43049f864e8ebb102
redpz <sv_redpz@proton.me>
2024-02-07 14:44:39 +0000

    Auto fold save

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -1,9 +1,9 @@
-return {{
+return { {
     'kevinhwang91/nvim-ufo',
     dependencies = 'kevinhwang91/promise-async',
-    config = function ()
+    config = function()
         vim.o.foldcolumn = '0' -- '0' is not bad
-        vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+        vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
         vim.o.foldlevelstart = 99
         vim.o.foldenable = true
 
@@ -23,6 +23,11 @@ return {{
                 -- you can add other fields for setting up lsp server in this table
             })
         end
-        require('ufo').setup()
+
+        require('ufo').setup({
+            provider_selector = function(bufnr, filetype, buftype)
+                return { 'lsp', 'indent' }
+            end
+        })
     end
-}}
+} }
