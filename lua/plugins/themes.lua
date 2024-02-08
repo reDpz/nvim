fd5e09cfd87e3e16bbeffc3280f62d4531ff3e11
reDpz <sv_redpz@proton.me>
2024-02-08 01:32:04 +0000

    m

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,6 +1,6 @@
 local load = true;
-local events = { "VeryLazy" }
-local cmd = { "Telescope" }
+local events = {"VeryLazy"}
+local cmd = {}
 
 return {
   {
@@ -23,7 +23,7 @@ return {
           -- Value is any valid attr-list value for `:help nvim_set_hl`
           comments = { italic = true },
           keywords = { italic = true },
-          functions = {},
+          functions = {italic = true},
           variables = {},
           -- Background styles. Can be "dark", "transparent" or "normal"
           sidebars = "dark",              -- style for sidebars, see below
@@ -38,13 +38,44 @@ return {
         --- You can override specific color groups to use other groups or a hex color
         --- function will be called with a ColorScheme table
         ---@param colors ColorScheme
-        on_colors = function(colors) end,
+        on_colors = function(colors)
+          colors.bg = "#1d1d25";
+        end,
 
         --- You can override specific highlights to use other groups or a hex color
         --- function will be called with a Highlights and ColorScheme table
-        ---@param highlights Highlights
-        ---@param colors ColorScheme
-        on_highlights = function(highlights, colors) end,
+
+        -- borderless telescope
+        on_highlights = function(hl, c)
+          local prompt = "#2d3149"
+          hl.TelescopeNormal = {
+            bg = c.bg_dark,
+            fg = c.fg_dark,
+          }
+          hl.TelescopeBorder = {
+            bg = c.bg_dark,
+            fg = c.bg_dark,
+          }
+          hl.TelescopePromptNormal = {
+            bg = prompt,
+          }
+          hl.TelescopePromptBorder = {
+            bg = prompt,
+            fg = prompt,
+          }
+          hl.TelescopePromptTitle = {
+            bg = prompt,
+            fg = prompt,
+          }
+          hl.TelescopePreviewTitle = {
+            bg = c.bg_dark,
+            fg = c.bg_dark,
+          }
+          hl.TelescopeResultsTitle = {
+            bg = c.bg_dark,
+            fg = c.bg_dark,
+          }
+        end,
       })
     end
   },
@@ -153,7 +184,7 @@ return {
             TelescopePromptPrefix = { bg = mocha.mantle },
             TelescopePromptNormal = { bg = mocha.mantle },
             TelescopePromptBorder = { bg = mocha.mantle, fg = mocha.mantle },
-            TelescopePromptTitle = { fg = mocha.base, bg = mocha.teal},
+            TelescopePromptTitle = { fg = mocha.base, bg = mocha.teal },
             -- Results
             TelescopeResultsNormal = { bg = mocha.crust },
             TelescopeResultsBorder = { bg = mocha.crust, fg = mocha.crust },
